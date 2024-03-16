fedd578166c37456a09f41e6e4e03cb09e6a63ac
jp117 <john.paradise117@gmail.com>
2024-03-16 09:47:14 -0400

    Wrote getComputerChoice() and getPlayerChoice()

--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 The Odin Project's Rock Paper Scissors using javascript in a browser console.
 
 ## Assignment
-1. ~~ Start a new Git repo for your project ~~
+1. ~~Start a new Git repo for your project~~
 2. ~~Create a blank HTML document with a script tag.  This game is going to be played completely from the console, so don't worry about putting anything else in there.~~
-3. Your game is going to play against the computer, so begin with a function called getComputerChoice that will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’. We’ll use this function in the game to make the computer’s play. Tip: use the console to make sure this is returning the expected output before moving to the next step!
+3. ~~Your game is going to play against the computer, so begin with a function called getComputerChoice that will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’. We’ll use this function in the game to make the computer’s play. Tip: use the console to make sure this is returning the expected output before moving to the next step!~~
 4. Write a function that plays a single round of Rock Paper Scissors. The function should take two parameters - the playerSelection and computerSelection - and then return a string that declares the winner or tie of the round like so: "You Lose! Paper beats Rock"
 
     * Make your function’s playerSelection parameter case-insensitive (so users can input rock, ROCK, RocK or any other variation).
---
+++ js/script.js
@@ -0,0 +1,45 @@
+// function getComputerChoice
+    //randomly return either 'Rock' 'Paper' or 'Scissors'
+function getComputerChoice(){
+    let computerChoice = Math.floor(Math.random()*3) + 1;
+    switch (computerChoice){
+        case 1:
+            return computerChoice = "rock";
+        case 2:
+            return computerChoice = "paper";
+        case 3:
+            return computerChoice = "scissors"
+        default:
+            return console.log(computerChoice);
+    }
+}
+
+//function getPlayerChoice
+    //Get the player's selection from the console. ** MUST BE CASE INSENSITIVE **
+function getPlayerChoice(){
+    let playerChoice = prompt("Please select 'r' for rock, 'p' for paper or 's' for scissors").toLowerCase();
+    let x = true;
+    while (x){
+        switch (playerChoice){
+            case 'r':
+                x = false;
+                return playerChoice = "rock"
+                break;
+            case 'p':
+                x = false;
+                return playerChoice = 'paper';
+            case 's':
+                x = false;
+                return playerChoice = 'scissors';
+            default:
+                playerChoice = prompt("Please select 'r' for rock, 'p' for paper or 's' for scissors");
+        }
+    }
+
+}
+// function playRound(playerSelection, computerSelection)
+    //Return a string of who won based on the input player selection and the random computer selection
+
+//function playFiveRounds
+    //keep score of overall winner and loser
+    
